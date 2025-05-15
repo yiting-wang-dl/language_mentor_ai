810e8b0961bfebf1ab1dc3d08fcbb1b035b2bc9d
Yiting Wang <ytwang82@gmail.com>
2025-05-14 19:44:31 -0700

    update gradio version & vocab tab

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # language_mentor_ai
+
+
+```bash
+# Create and activate a virtual environment
+python3 -m venv language_mentor_venv
+source language_mentor_venv/bin/activate
+pip install -r requirements.txt
+
+# Start the application locally
+python src/main.py
+
+# Launch the Gradio UI
+# http://localhost:7860
+
+```
+
+in .env file, set the following:
+
+AI_BACKEND=openai
+OPENAI_API_KEY=sk-your-key-here
+
+
+pip install python-dotenv
+
+
+
+**Gradio UI**
+   open `http://localhost:7860` to start the application.
+
+   vocab：
+   ![gradio_demo_0](images/gradio_vocab_0.png)
+   ![gradio_demo_1](images/gradio_vocab_1.png)
+
--- src/tabs/vocab_tab.py
+++ src/tabs/vocab_tab.py
@@ -22,13 +22,12 @@ def restart_vocab_study_chatbot():
     vocab_agent.restart_session()
 
     # Define initial message and interact with vocab agent
-    _next_round = "Let's do it"
+    _next_round = "Start learning new words!"
     bot_message = vocab_agent.chat_with_history(_next_round)    
 
     return gr.Chatbot(
         value=[(_next_round, bot_message)],
-        height=800,
-        show_progress=False,
+        height=1000,
     )
 
 def handle_vocab(user_input, chat_history):
@@ -42,36 +41,35 @@ def create_vocab_tab():
 
         gr.Markdown(get_page_desc(feature))
 
-        # vocab_chatbot = gr.Chatbot(
-        #     placeholder="<strong>Your English Mentor Eva</strong><br><br>Start learning new words!",
-        #     height=800,
-        # )   
+        vocab_chatbot = gr.Chatbot(
+            placeholder="<strong>Let's do it!</strong>",
+            height=800,
+        )   
 
-        # restart_btn = gr.ClearButton(value="Next Level")
+        gr.ChatInterface(
+            # type="messages",  # TODO: tuple will be deprecated, updated to message! message doesn't work with current implementation.
+            fn=handle_vocab,
+            chatbot=vocab_chatbot,
+            submit_btn="Send",  
+            title="<strong>Your English Mentor Eva</strong><br><br>Start learning new words!",
+        )
 
-        # restart_btn.click(
-        #     fn=restart_vocab_study_chatbot,
-        #     inputs=None,
-        #     outputs=vocab_chatbot,
-        # )
+        restart_btn = gr.ClearButton(value="Get Started!")
 
+        restart_btn.click(
+            fn=restart_vocab_study_chatbot,
+            inputs=None,
+            outputs=vocab_chatbot,
+        )
+
+        
         # gr.ChatInterface(
         #     fn=handle_vocab,
-        #     # chatbot=vocab_chatbot,
+        #     submit_btn="Send",
         #     retry_btn=None,
         #     undo_btn=None,
-        #     clear_btn=None,
-        #     submit_btn="Send",  
+        #     clear_btn="Next Level",
         #     title="<strong>你的英语私教 Eva</strong><br><br>Start learning new words!",
         # )
-        
-        gr.ChatInterface(
-            fn=handle_vocab,
-            submit_btn="Send",
-            retry_btn=None,
-            undo_btn=None,
-            clear_btn="Next Level",
-            title="<strong>你的英语私教 Eva</strong><br><br>Start learning new words!",
-        )
     
 
