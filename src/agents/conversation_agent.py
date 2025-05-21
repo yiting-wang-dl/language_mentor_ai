5d72f5f852a9e769fe1793f8415ebed161cc0268
Yiting Wang <ytwang82@gmail.com>
2025-05-20 21:38:28 -0700

    fix conversation clear button

--- src/agents/conversation_agent.py
+++ src/agents/conversation_agent.py
@@ -1,3 +1,4 @@
+import random
 from langchain_core.messages import AIMessage 
 
 from .session_history import get_session_history
@@ -15,7 +16,28 @@ class ConversationAgent(AgentBase):
             session_id=session_id
             )
         
+    def start_new_session(self, session_id=None):
+        """
+        Start a new conversation session and send a random initial AI message.
 
+        Args:
+            session_id (str, optional): Unique identifier for the session.
+
+        Returns:
+            str: Initial AI message
+        """
+        if session_id is None:
+            session_id = self.session_id
+
+        history = get_session_history(session_id)
+        LOG.debug(f"[history][{session_id}]:{history}")
+
+        if not history.messages:
+            initial_ai_message = random.choice(self.intro_messages)
+            history.add_message(AIMessage(content=initial_ai_message))
+            return initial_ai_message
+        else:
+            return history.messages[-1].content
 
 # from langchain_ollama.chat_models import ChatOllama
 # from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
--- src/tabs/conversation_tab.py
+++ src/tabs/conversation_tab.py
@@ -7,14 +7,26 @@ from utils.logger import LOG
 
 conversation_agent = ConversationAgent()
 
-def handle_conversation(user_input, chat_history):
-    bot_message = conversation_agent.chat_with_history(user_input)
-    LOG.info(f"[Conversation ChatBot]: {bot_message}")
-    return bot_message
+def restart_conversation_chatbot():
+    conversation_agent.start_new_session()
+
+    # Define initial message and interact with vocab agent
+    _next_round = "Start new conversation!"
+    bot_message =conversation_agent.chat_with_history(_next_round)    
+
+    return gr.Chatbot(
+        value=[(_next_round, bot_message)],
+        height=600,
+    )
 
 def create_conversation_tab():
     with gr.Tab("Conversation"):
         gr.Markdown("## Practice English Conversation")
+
+        conversation_chatbot = gr.Chatbot(
+            placeholder="<strong>Your English Mentor Eva</strong><br><br>You can talk to me about anything, remember to use English!",
+            height=800, 
+        )
 
         def handle_conversation(user_input, chat_history):
             bot_message = conversation_agent.chat_with_history(user_input)
@@ -22,11 +34,17 @@ def create_conversation_tab():
             return bot_message
         
         gr.ChatInterface(
+            # type="messages",  # TODO: tuple will be deprecated, updated to message! message doesn't work with current implementation.
             fn=handle_conversation,
-            retry_btn=None, # Hide retry button
-            undo_btn=None, # Hide undo button
-            clear_btn="Clear History",
-            title="Your English Mentor Eva",
-            description="You can talk to me about anything, remember to use English!",
+            chatbot=conversation_chatbot,
             submit_btn="Send",
+            title="Your English Mentor Eva",
+        )
+
+        # TODO: history is not correctly cleared. 
+        clear_btn = gr.ClearButton(value="Clear History")
+        clear_btn.click(
+            fn=restart_conversation_chatbot, 
+            inputs=None,
+            outputs=conversation_chatbot,
         )
